b137e04f744492e0564439a22e2ccf397c4391dd
TopoFabbri <matufabbri@gmail.com>
2024-05-01 06:07:19 -0300

    Improved drone behaviour

--- Assets/Follow.cs
+++ Assets/Follow.cs
@@ -14,9 +14,18 @@ public class Follow : MonoBehaviour
         float targetAngle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90f;
         Quaternion targetRotation = Quaternion.Euler(0, 0, targetAngle);
 
-        transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, maxRotationSpeed * Time.deltaTime);
+        transform.rotation =
+            Quaternion.RotateTowards(transform.rotation, targetRotation, maxRotationSpeed * Time.deltaTime);
+
+        Vector2 diff = (target.position - transform.position).normalized;
 
         if (Vector2.Distance(transform.position, target.position) > stopDistance)
             transform.position = Vector2.MoveTowards(transform.position, target.position, maxSpeed * Time.deltaTime);
+        else if (Vector2.Distance(transform.position, target.position) < stopDistance)
+            transform.position = Vector2.MoveTowards(transform.position, (Vector2)target.position - diff * stopDistance,
+                maxSpeed * Time.deltaTime);
+
+        if (transform.position.y < target.position.y)
+            transform.position = Vector2.MoveTowards(transform.position, new Vector2(transform.position.x, target.position.y), maxSpeed * Time.deltaTime);
     }
 }
