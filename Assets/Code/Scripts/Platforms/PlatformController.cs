dbec9693d36c6aaa9f5b7d44c0f6f42bd99e3e08
TopoFabbri <matufabbri@gmail.com>
2024-06-05 18:09:29 -0300

    Implemented platform animation

---
+++ Assets/Code/Scripts/Platforms/PlatformController.cs
@@ -0,0 +1,36 @@
+using Code.Scripts.Colors;
+using UnityEngine;
+
+namespace Code.Scripts.Platforms
+{
+    public class PlatformController : MonoBehaviour
+    {
+        [SerializeField] private Animator animator;
+        [SerializeField] private ColorSwitcher.QColor qColor;
+        
+        private static readonly int On = Animator.StringToHash("On");
+
+        private void OnEnable()
+        {
+            ColorSwitcher.ColorChanged += ToggleColor;
+        }
+
+        private void ToggleColor(ColorSwitcher.QColor obj)
+        {
+            if (obj == qColor || qColor == ColorSwitcher.QColor.None)
+                Activate();
+            else
+                Deactivate();
+        }
+
+        private void Activate()
+        {
+            animator.SetBool(On, true);
+        }
+
+        private void Deactivate()
+        {
+            animator.SetBool(On, false);
+        }
+    }
+}
