ab5ad22b05d7554ef78b1762905704a98e5a43aa
gytsss <totogodoy@hotmail.com>
2024-08-22 15:10:53 -0300

    Add options panel

--- Assets/Code/Scripts/Menu/MenuController.cs
+++ Assets/Code/Scripts/Menu/MenuController.cs
@@ -13,6 +13,8 @@ namespace Code.Scripts.Menu
         [SerializeField] private string level2SceneName;
         [SerializeField] private string level3SceneName;
         [SerializeField] private string level4SceneName;
+        [SerializeField] private GameObject optionsPanel;
+        [SerializeField] private GameObject mainMenuButtons;
 
         private void Start()
         {
@@ -29,41 +31,52 @@ namespace Code.Scripts.Menu
         {
             LoadScene(levelSelectorScene);
         }
-    
+
         public void ShowCredits()
         {
             LoadScene(creditsSceneName);
         }
-    
+
+        public void TurnOptions()
+        {
+            optionsPanel.SetActive(!optionsPanel.activeSelf);
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+
+            if (optionsPanel.activeSelf)
+                mainMenuButtons.SetActive(false);
+            else
+                mainMenuButtons.SetActive(true);
+        }
+
         public void GoMainMenu()
         {
             LoadScene(mainMenuSceneName);
         }
-    
+
         public void LoadLevel1()
         {
             LoadScene(level1SceneName);
         }
-    
+
         public void LoadLevel2()
         {
             LoadScene(level2SceneName);
         }
-    
+
         public void LoadLevel3()
         {
             LoadScene(level3SceneName);
         }
-    
+
         public void LoadLevel4()
         {
             LoadScene(level4SceneName);
         }
-    
+
         public void QuitGame()
         {
             Application.Quit();
         }
-    
     }
 }
