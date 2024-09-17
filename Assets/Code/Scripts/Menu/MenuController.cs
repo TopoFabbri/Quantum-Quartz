9cc229fc6db85498eabbda4084bf03f745e526fa
gytsss <totogodoy@hotmail.com>
2024-09-17 14:26:14 -0300

    Update options controller

--- Assets/Code/Scripts/Menu/MenuController.cs
+++ Assets/Code/Scripts/Menu/MenuController.cs
@@ -15,28 +15,13 @@ namespace Code.Scripts.Menu
         [SerializeField] private string level2SceneName;
         [SerializeField] private string level3SceneName;
         [SerializeField] private string level4SceneName;
-        [SerializeField] private GameObject optionsPanel;
-        [SerializeField] private GameObject creditsPanel;
-        [SerializeField] private GameObject controlsPanel;
-        [SerializeField] private GameObject videoPanel;
-        [SerializeField] private GameObject audioPanel;
-        [SerializeField] private GameObject mainMenuButtons;
+        
         [SerializeField] private GameObject fadeOut;
-        [SerializeField] private Button optionsFirstButton;
-        [SerializeField] private Button creditsBackButton;
-        [SerializeField] private Button controlsBackButton;
-        [SerializeField] private Button videoFirstButton;
-        [SerializeField] private Button audioFirstButton;
-        [SerializeField] private Button mainMenuButton;
-
+        
 
         private void Start()
         {
-            optionsPanel.SetActive(false);
-            creditsPanel.SetActive(false);
-            controlsPanel.SetActive(false);
-            videoPanel.SetActive(false);
-            audioPanel.SetActive(false);
+            
 
             Cursor.lockState = CursorLockMode.Locked;
             Cursor.visible = false;
@@ -52,85 +37,7 @@ namespace Code.Scripts.Menu
             LoadScene(levelSelectorScene);
         }
 
-        public void TurnCredits()
-        {
-            creditsPanel.SetActive(!creditsPanel.activeSelf);
-            
-            if (creditsPanel.activeSelf)
-            {
-                mainMenuButtons.SetActive(false);
-                creditsBackButton.Select();
-            }
-            else
-            {
-                mainMenuButtons.SetActive(true);
-                mainMenuButton.Select();
-            }
-        }
-
-        public void TurnOptions()
-        {
-            optionsPanel.SetActive(!optionsPanel.activeSelf);
-
-            if (optionsPanel.activeSelf)
-            {
-                mainMenuButtons.SetActive(false);
-                optionsFirstButton.Select();
-            }
-            else
-            {
-                mainMenuButtons.SetActive(true);
-                mainMenuButton.Select();
-            }
-        }
-
-        public void TurnControls()
-        {
-            controlsPanel.SetActive(!controlsPanel.activeSelf);
-
-            if (controlsPanel.activeSelf)
-            {
-                optionsPanel.SetActive(false);
-                controlsBackButton.Select();
-            }
-            else
-            {
-                optionsPanel.SetActive(true);
-                optionsFirstButton.Select();
-            }
-        }
-
-        public void TurnVideo()
-        {
-            videoPanel.SetActive(!videoPanel.activeSelf);
-
-            if (videoPanel.activeSelf)
-            {
-                optionsPanel.SetActive(false);
-                videoFirstButton.Select();
-            }
-            else
-            {
-                optionsPanel.SetActive(true);
-                optionsFirstButton.Select();
-            }
-        }
         
-        public void TurnAudio()
-        {
-            audioPanel.SetActive(!audioPanel.activeSelf);
-
-            if (audioPanel.activeSelf)
-            {
-                optionsPanel.SetActive(false);
-                audioFirstButton.Select();
-            }
-            else
-            {
-                optionsPanel.SetActive(true);
-                optionsFirstButton.Select();
-            }
-        }
         
         public void GoMainMenu()
         {
--- Assets/Code/Scripts/Menu/OptionsController.cs
+++ Assets/Code/Scripts/Menu/OptionsController.cs
@@ -1,11 +1,115 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class OptionsController : MonoBehaviour
 {
+    [SerializeField] private GameObject optionsPanel;
+    [SerializeField] private GameObject creditsPanel;
+    [SerializeField] private GameObject controlsPanel;
+    [SerializeField] private GameObject videoPanel;
+    [SerializeField] private GameObject audioPanel;
+    [SerializeField] private GameObject mainMenuButtons;
+    
+    [SerializeField] private Button optionsFirstButton;
+    [SerializeField] private Button creditsBackButton;
+    [SerializeField] private Button controlsBackButton;
+    [SerializeField] private Button videoFirstButton;
+    [SerializeField] private Button audioFirstButton;
+    [SerializeField] private Button mainMenuButton;
+
+    private void Start()
+    {
+        optionsPanel.SetActive(false);
+        creditsPanel.SetActive(false);
+        controlsPanel.SetActive(false);
+        videoPanel.SetActive(false);
+        audioPanel.SetActive(false);
+    }
+
     public void SetFullScreen()
     {
         Screen.fullScreen = !Screen.fullScreen;
     }
+    public void TurnCredits()
+        {
+            creditsPanel.SetActive(!creditsPanel.activeSelf);
+            
+            if (creditsPanel.activeSelf)
+            {
+                mainMenuButtons.SetActive(false);
+                creditsBackButton.Select();
+            }
+            else
+            {
+                mainMenuButtons.SetActive(true);
+                mainMenuButton.Select();
+            }
+        }
+
+        public void TurnOptions()
+        {
+            optionsPanel.SetActive(!optionsPanel.activeSelf);
+
+            if (optionsPanel.activeSelf)
+            {
+                mainMenuButtons.SetActive(false);
+                optionsFirstButton.Select();
+            }
+            else
+            {
+                mainMenuButtons.SetActive(true);
+                mainMenuButton.Select();
+            }
+        }
+
+        public void TurnControls()
+        {
+            controlsPanel.SetActive(!controlsPanel.activeSelf);
+
+            if (controlsPanel.activeSelf)
+            {
+                optionsPanel.SetActive(false);
+                controlsBackButton.Select();
+            }
+            else
+            {
+                optionsPanel.SetActive(true);
+                optionsFirstButton.Select();
+            }
+        }
+
+        public void TurnVideo()
+        {
+            videoPanel.SetActive(!videoPanel.activeSelf);
+
+            if (videoPanel.activeSelf)
+            {
+                optionsPanel.SetActive(false);
+                videoFirstButton.Select();
+            }
+            else
+            {
+                optionsPanel.SetActive(true);
+                optionsFirstButton.Select();
+            }
+        }
+        
+        public void TurnAudio()
+        {
+            audioPanel.SetActive(!audioPanel.activeSelf);
+
+            if (audioPanel.activeSelf)
+            {
+                optionsPanel.SetActive(false);
+                audioFirstButton.Select();
+            }
+            else
+            {
+                optionsPanel.SetActive(true);
+                optionsFirstButton.Select();
+            }
+        }
 }
