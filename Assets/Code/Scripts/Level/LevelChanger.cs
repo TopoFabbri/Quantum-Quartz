9e3d1bf65bfc220d418f4d91544b743558b923d0
tobi <totogodoy@hotmail.com>
2024-10-15 20:25:29 -0300

    Add end level screen(Work in progress)

--- Assets/Code/Scripts/Level/LevelChanger.cs
+++ Assets/Code/Scripts/Level/LevelChanger.cs
@@ -2,6 +2,7 @@ using System;
 using Code.Scripts.Tools;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 namespace Code.Scripts.Level
 {
@@ -9,17 +10,32 @@ namespace Code.Scripts.Level
     {
         [SerializeField] private string[] levelScenes;
         [SerializeField] private int currentLevel;
+        [SerializeField] private Canvas endLevelCanvas;
+        [SerializeField] private Button endLevelFirstSelectedButton;
 
         public int CurrentLevel => currentLevel;
 
         public static event Action LevelEnd;
         public static event Action PlayerTp;
 
+        private void Start()
+        {
+            //Time.timeScale = 1;
+            endLevelCanvas.gameObject.SetActive(false);
+        }
+
         private void OnTriggerEnter2D(Collider2D other)
         {
             if (other.CompareTag("Player"))
+            {
+                //endLevelCanvas.gameObject.SetActive(true);
+                //endLevelFirstSelectedButton.Select();
+                //Time.timeScale = 0;
                 PlayerTp?.Invoke();
+            }
         }
+        
+        
 
         public static void EndLevel()
         {
