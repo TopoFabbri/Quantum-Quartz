c449cb0b3fa7d2277b53642685e7a2ea10aeacd3
TopoFabbri <matufabbri@gmail.com>
2024-11-18 18:53:01 -0300

    Added UI platform animations

--- Assets/Code/Scripts/Menu/LevelSelectorScreenController.cs
+++ Assets/Code/Scripts/Menu/LevelSelectorScreenController.cs
@@ -1,69 +1,71 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class LevelSelectorScreenController : MonoBehaviour
+namespace Code.Scripts.Menu
 {
-     public RectTransform contentPanel; 
-    public float slideDuration = 0.5f; 
-
-    private RectTransform selectedButton; 
-    private Vector2 targetPosition; 
-    private bool isSliding = false; 
-
-    void Update()
+    public class LevelSelectorScreenController : MonoBehaviour
     {
-        GameObject currentSelected = EventSystem.current.currentSelectedGameObject;
+        public RectTransform contentPanel;
+        public float slideDuration = 0.5f;
 
-        if (currentSelected != null && currentSelected.transform.parent == contentPanel)
+        private RectTransform selectedButton;
+        private Vector2 targetPosition;
+        private bool isSliding = false;
+
+        void Update()
         {
-            RectTransform newSelectedButton = currentSelected.GetComponent<RectTransform>();
+            GameObject currentSelected = EventSystem.current.currentSelectedGameObject;
 
-            if (newSelectedButton != selectedButton)
+            if (currentSelected != null && currentSelected.transform.parent == contentPanel)
             {
-                selectedButton = newSelectedButton;
-                UpdateTargetPosition();
+                RectTransform newSelectedButton = currentSelected.GetComponent<RectTransform>();
+
+                if (newSelectedButton != selectedButton)
+                {
+                    selectedButton = newSelectedButton;
+                    UpdateTargetPosition();
+                }
+            }
+
+            if (isSliding)
+            {
+                contentPanel.anchoredPosition = Vector2.Lerp(contentPanel.anchoredPosition, targetPosition,
+                    Time.deltaTime / slideDuration);
+
+                if (Vector2.Distance(contentPanel.anchoredPosition, targetPosition) < 0.1f)
+                {
+                    contentPanel.anchoredPosition = targetPosition;
+                    isSliding = false;
+                }
             }
         }
 
-        if (isSliding)
+        private void UpdateTargetPosition()
         {
-            contentPanel.anchoredPosition = Vector2.Lerp(contentPanel.anchoredPosition, targetPosition, Time.deltaTime / slideDuration);
+            float contentWidth = contentPanel.rect.width;
+            float buttonWidth = selectedButton.rect.width;
+            float buttonSpacing = GetSpacing();
+            float paddingLeft = GetPaddingLeft();
 
-            if (Vector2.Distance(contentPanel.anchoredPosition, targetPosition) < 0.1f)
-            {
-                contentPanel.anchoredPosition = targetPosition;
-                isSliding = false;
-            }
+            float offset = (contentWidth / 2f) - (buttonWidth / 2f);
+            float buttonCenterX = -selectedButton.anchoredPosition.x + offset + paddingLeft + buttonSpacing;
+
+            targetPosition = new Vector2(buttonCenterX, contentPanel.anchoredPosition.y);
+
+            isSliding = true;
         }
-    }
 
-    private void UpdateTargetPosition()
-    {
-        float contentWidth = contentPanel.rect.width; 
-        float buttonWidth = selectedButton.rect.width; 
-        float buttonSpacing = GetSpacing();
-        float paddingLeft = GetPaddingLeft(); 
+        private float GetSpacing()
+        {
+            HorizontalLayoutGroup layoutGroup = contentPanel.GetComponent<HorizontalLayoutGroup>();
+            return layoutGroup != null ? layoutGroup.spacing : 0f;
+        }
 
-        float offset = (contentWidth / 2f) - (buttonWidth / 2f);
-        float buttonCenterX = -selectedButton.anchoredPosition.x + offset + paddingLeft + buttonSpacing;
-
-        targetPosition = new Vector2(buttonCenterX, contentPanel.anchoredPosition.y);
-
-        isSliding = true;
-    }
-
-    private float GetSpacing()
-    {
-        HorizontalLayoutGroup layoutGroup = contentPanel.GetComponent<HorizontalLayoutGroup>();
-        return layoutGroup != null ? layoutGroup.spacing : 0f;
-    }
-
-    private float GetPaddingLeft()
-    {
-        HorizontalLayoutGroup layoutGroup = contentPanel.GetComponent<HorizontalLayoutGroup>();
-        return layoutGroup != null ? layoutGroup.padding.left : 0f;
+        private float GetPaddingLeft()
+        {
+            HorizontalLayoutGroup layoutGroup = contentPanel.GetComponent<HorizontalLayoutGroup>();
+            return layoutGroup != null ? layoutGroup.padding.left : 0f;
+        }
     }
 }
