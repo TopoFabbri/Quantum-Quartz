c4d7a188ae3c3a21fdb735a015acaf19c0ee28cc
TopoFabbri <matufabbri@gmail.com>
2024-11-18 22:55:22 -0300

    Reworked wall state facing sides

--- Assets/Code/Scripts/States/WallState.cs
+++ Assets/Code/Scripts/States/WallState.cs
@@ -12,7 +12,8 @@ namespace Code.Scripts.States
         private WallSettings WallSettings => settings as WallSettings;
 
         private float savedGravityScale;
-
+        private RaycastHit2D wallContact;
+        
         public bool FacingRight { get; set; }
 
         public WallState(T id, StateSettings.StateSettings stateSettings, Rigidbody2D rb, Transform transform,
@@ -43,7 +44,6 @@ namespace Code.Scripts.States
 
             savedGravityScale = rb.gravityScale;
             
-            PositionPlayer();
             mb.StartCoroutine(SpawnDusts());
         }
 
@@ -60,7 +60,7 @@ namespace Code.Scripts.States
         /// Check if player is touching a wall
         /// </summary>
         /// <returns>True if touching a wall</returns>
-        public bool CanWallJump()
+        public bool CanEnterWall()
         {
             if (ColorSwitcher.Instance.CurrentColor != ColorSwitcher.QColor.Green)
                 return false;
@@ -87,12 +87,39 @@ namespace Code.Scripts.States
         }
 
         /// <summary>
+        /// Check if player is touching a wall
+        /// </summary>
+        /// <returns> True if touching a wall</returns>
+        public bool IsTouchingWall()
+        {
+            Vector2 pos = (Vector2)transform.position +
+                          Vector2.right * (FacingRight ? -WallSettings.wallCheckDis : WallSettings.wallCheckDis);
+
+            Collider2D[] colliders =
+                Physics2D.OverlapBoxAll(pos, moveSettings.wallCheckSize, 0, LayerMask.GetMask("Default"));
+
+            foreach (Collider2D collider in colliders)
+            {
+                if (collider.gameObject.CompareTag("Wall") || collider.gameObject.CompareTag("Floor"))
+                    return true;
+
+                if (!collider.gameObject.CompareTag("Platform")) continue;
+
+                if (collider.TryGetComponent(out PlatformEffector2D platformEffector2D)) continue;
+                
+                return true;
+            }
+
+            return false;
+        }
+        
+        /// <summary>
         /// Place player at a certain distance from the wall
         /// </summary>
         private void PositionPlayer()
         {
-            RaycastHit2D hit = Physics2D.Raycast(transform.position + (FacingRight ? Vector3.left : Vector3.right),
-                FacingRight ? Vector2.right : Vector2.left, WallSettings.wallCheckDis * 4f,
+            RaycastHit2D hit = Physics2D.Raycast(transform.position + (FacingRight ? Vector3.right : Vector3.left),
+                FacingRight ? Vector2.left : Vector2.right, WallSettings.wallCheckDis * 4f,
                 LayerMask.GetMask("Default"));
             
             if (!hit.collider || !hit.collider.CompareTag("Floor") && !hit.collider.CompareTag("Platform"))
@@ -102,7 +129,7 @@ namespace Code.Scripts.States
                 objMovement.AddPlayer(transform);
             
             Vector3 newPos = transform.position;
-            newPos.x = hit.point.x + (FacingRight ? -1f : 1f) * WallSettings.wallDis;
+            newPos.x = hit.point.x + (FacingRight ? 1f : -1f) * WallSettings.wallDis;
 
             transform.position = newPos;
         }
