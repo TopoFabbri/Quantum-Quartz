5a0c42d8d6a9b42560c75862a18a28d25c67c79f
tatoyoda600 <26845479+tatoyoda600@users.noreply.github.com>
2025-06-25 20:39:33 -0300

    Conversation Scriptable Object

---
+++ Assets/Code/Scripts/Dialogue/Conversation.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Localization;
+
+namespace Code.Scripts.Dialogue
+{
+    [CreateAssetMenu(menuName = "Custom/Conversation", fileName = "Conversation", order = 1)]
+    public class Conversation : ScriptableObject
+    {
+        public enum PortraitAnimation
+        {
+            None,
+            Molly_Normal,
+            Molly_Happy,
+            Molly_Sad
+        }
+
+        [System.Serializable]
+        public struct TextBox
+        {
+            public LocalizedString text;
+            public PortraitAnimation portrait;
+        }
+
+        public List<TextBox> textBoxes;
+    }
+}
