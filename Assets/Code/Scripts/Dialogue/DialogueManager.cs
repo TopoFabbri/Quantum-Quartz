65f6a56c0bd963e5aa6b9319e0231e25fa03f9dc
tatoyoda600 <26845479+tatoyoda600@users.noreply.github.com>
2025-06-30 19:05:28 -0300

    Letter by letter typing, and animation pause

--- Assets/Code/Scripts/Dialogue/DialogueManager.cs
+++ Assets/Code/Scripts/Dialogue/DialogueManager.cs
@@ -9,6 +9,9 @@ namespace Code.Scripts.Dialogue
 {
     public class DialogueManager : MonoBehaviourSingleton<DialogueManager>
     {
+        private const float CHAR_DELAY = 0.05f;
+        private const float FAST_CHAR_DELAY = 0.001f;
+
         [SerializeField] private Animator dialoguePanelAnim;
         [SerializeField] private TextMeshProUGUI dialogueText;
 
@@ -34,8 +37,44 @@ namespace Code.Scripts.Dialogue
 
             foreach (Conversation.TextBox textBox in conversation.textBoxes)
             {
-                dialogueText.text = textBox.Text;
+                dialoguePanelAnim.speed = 1;
                 dialoguePanelAnim.SetInteger("Portrait", (int)textBox.portrait);
+
+                for (int i = 1; i < textBox.Text.Length; i++)
+                {
+                    // If empty char, skip
+                    if (!string.IsNullOrWhiteSpace(textBox.Text[i].ToString()))
+                    {
+                        // Since '\' can be used to escape '>' and '<' symbols, have to skip over the invisible '\'
+                        if (textBox.Text[i - 1] == '\\' && (textBox.Text[i] == '<' || textBox.Text[i] == '>'))
+                        {
+                            i++;
+                        }
+
+                        // If just processed a '<', skip ahead to next '>'
+                        if (textBox.Text[i - 1] == '<')
+                        {
+                            // Advance until exceeding text length, or finding a '>' that isn't preceded by a '\'
+                            while (i < textBox.Text.Length && textBox.Text[i] != '>' && textBox.Text[i - 1] != '\\')
+                            {
+                                i++;
+                            }
+                        }
+                        else
+                        {
+                            dialogueText.text = textBox.Text.Insert(i, "<color=#00000000>").Replace("\\>", ">").Replace("\\<", "<") + "</color>";
+                            yield return new WaitForSeconds(skipText ? FAST_CHAR_DELAY : CHAR_DELAY);
+                        }
+                    }
+                }
+
+                skipText = false;
+                dialoguePanelAnim.speed = 0;
+                for (int i = 0; i < dialoguePanelAnim.layerCount; i++)
+                {
+                    dialoguePanelAnim.Play(dialoguePanelAnim.GetCurrentAnimatorStateInfo(i).shortNameHash, i, 0);
+                }
+                dialogueText.text = textBox.Text.Replace("\\>", ">").Replace("\\<", "<");
                 yield return new WaitUntil(() => skipText);
                 skipText = false;
             }
