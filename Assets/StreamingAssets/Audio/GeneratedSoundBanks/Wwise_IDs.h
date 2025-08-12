56c7fbf4b0b002c2f5bc3636b901caeb587162df
Flippax <goizuetafelipe@gmail.com>
2025-08-12 19:56:03 -0300

    Revert "Added AquaticMX"
    This reverts commit 88189737b18821fd1458c70f469319f793210731.

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -136,7 +136,6 @@ namespace AK
             namespace SWITCH
             {
                 static const AkUniqueID ADVENTURER = 3247222823U;
-                static const AkUniqueID AQUATIC = 3653896563U;
                 static const AkUniqueID GHOST = 4023194814U;
                 static const AkUniqueID MAINTHEME = 824317709U;
                 static const AkUniqueID NOMX = 732117587U;
