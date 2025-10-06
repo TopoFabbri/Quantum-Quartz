a38810dfb6c02e4c702fe56f67c36e91d67675ec
tatoyoda600 <26845479+tatoyoda600@users.noreply.github.com>
2025-10-06 19:54:35 -0300

    Fix RedUnlock and make interactable

--- Assets/Code/Scripts/Game/Triggers/CutsceneTrigger.cs
+++ Assets/Code/Scripts/Game/Triggers/CutsceneTrigger.cs
@@ -5,14 +5,22 @@ namespace Code.Scripts.Game.Triggers
 {
     public class CutsceneTrigger : InteractableComponent
     {
+        [SerializeField] private bool interactable = false;
         [SerializeField] private string triggerName = "Enter trigger name";
         [SerializeField] private bool reusable = false;
+        [SerializeField] private GameObject targetHighlight;
+
+        public override bool RequiresClick => interactable;
 
         private void Awake()
         {
             foreach (SpriteRenderer sprite in gameObject.GetComponents<SpriteRenderer>())
             {
                 sprite.enabled = false;
+            }
+            if (targetHighlight)
+            {
+                targetHighlight.SetActive(false);
             }
         }
 
@@ -29,6 +37,15 @@ namespace Code.Scripts.Game.Triggers
             if (!reusable)
             {
                 gameObject.SetActive(false);
+            }
+        }
+
+        protected override void OnAwaitingInteraction(bool awaitingInteraction)
+        {
+            base.OnAwaitingInteraction(awaitingInteraction);
+            if (targetHighlight)
+            {
+                targetHighlight.SetActive(awaitingInteraction);
             }
         }
     }
