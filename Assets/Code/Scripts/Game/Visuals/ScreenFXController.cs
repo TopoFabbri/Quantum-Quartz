708a04e95e475ccdbc90539935c68bb84b5e2db4
tatoyoda600 <26845479+tatoyoda600@users.noreply.github.com>
2025-10-16 22:41:31 -0300

    Fix wrong wwise event type

--- Assets/Code/Scripts/Game/Visuals/ScreenFXController.cs
+++ Assets/Code/Scripts/Game/Visuals/ScreenFXController.cs
@@ -11,7 +11,7 @@ namespace Code.Scripts.Game.Visuals
     {
         [SerializeField] private Animator animator;
         [SerializeField] private WwiseEvent playGearEvent;
-        [SerializeField] private WwiseEvent playLevelFinishedEvent;
+        [SerializeField] private AK.Wwise.Event playLevelFinishedEvent;
 
         private static readonly int Ended = Animator.StringToHash("Ended");
         private static readonly int SwitchedColor = Animator.StringToHash("SwitchedColor");
@@ -33,7 +33,7 @@ namespace Code.Scripts.Game.Visuals
         /// </summary>
         private void End()
         {
-            playLevelFinishedEvent.SetOn(gameObject);
+            playLevelFinishedEvent.Post(gameObject);
             animator.SetBool(Ended, true);
         }
 
