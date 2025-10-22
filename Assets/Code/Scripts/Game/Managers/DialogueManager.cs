3bc45c16f2b45d6437159dca758a50351f1b51ba
tatoyoda600 <26845479+tatoyoda600@users.noreply.github.com>
2025-10-22 20:10:07 -0300

    Add custom tag handling for dialogues

--- Assets/Code/Scripts/Game/Managers/DialogueManager.cs
+++ Assets/Code/Scripts/Game/Managers/DialogueManager.cs
@@ -18,13 +18,29 @@ namespace Code.Scripts.Game.Managers
         private const float CHAR_DELAY = 0.05f;
         private const float FAST_CHAR_DELAY = 0.0005f;
 
-        [System.Serializable]
-        public class KeywordMapping
+        public abstract class DialogueTagMapping
         {
             [SerializeField] private string keyword;
-            [SerializeField] private SerializedDictionary<InputManager.ControlScheme, string> mappings;
             public string Keyword => keyword;
+        }
+
+        [System.Serializable]
+        public class ButtonMapping : DialogueTagMapping
+        {
+            [SerializeField] private SerializedDictionary<InputManager.ControlScheme, string> mappings;
             public IReadOnlyDictionary<InputManager.ControlScheme, string> Mappings => mappings;
+        }
+
+        [System.Serializable]
+        public class SpecialMapping : DialogueTagMapping
+        {
+            public enum SpecialEffects
+            {
+                Delay
+            }
+
+            [SerializeField] private SerializedDictionary<SpecialEffects, string> mappings;
+            public IReadOnlyDictionary<SpecialEffects, string> Mappings => mappings;
         }
 
         [SerializeField] private float portraitOffset = 352;
@@ -34,7 +50,8 @@ namespace Code.Scripts.Game.Managers
         [SerializeField] private RectTransform textBox;
         [SerializeField] private PauseController pauseController;
         [SerializeField] private SerializedDictionary<Conversation.PortraitAnimation, Conversation.PortraitAlignment> portraitAlignments;
-        [SerializeField] private List<KeywordMapping> keywordMappings;
+        [SerializeField] private List<ButtonMapping> buttonMappings;
+        [SerializeField] private List<SpecialMapping> specialMappings;
 
         private bool advanceText = false;
         private Coroutine curDialogue = null;
@@ -70,6 +87,8 @@ namespace Code.Scripts.Game.Managers
             onCurDialogueEnd = onConversationEnd;
         }
 
+        static readonly Regex WHITESPACE = new Regex(@"\s+", RegexOptions.IgnoreCase);
+        static readonly Regex TAG_ATTR_SEPARATION = new Regex(@"(?<!=)\s+(?!=)", RegexOptions.IgnoreCase);
         IEnumerator DialogueLoop(Conversation conversation)
         {
             pauseController.EnterDialogue();
@@ -101,25 +120,89 @@ namespace Code.Scripts.Game.Managers
                         // If just processed a '<', skip ahead to next '>'
                         if (textBox.Text[i - 1] == '<')
                         {
+                            int startBracket = i - 1;
                             // Advance until exceeding text length, or finding a '>' that isn't preceded by a '\'
                             while (i < textBox.Text.Length && textBox.Text[i] != '>' && textBox.Text[i - 1] != '\\')
                             {
                                 i++;
                             }
+
+                            // Custom tag handling for "special" tags
+                            string tagContent = textBox.Text.Substring(startBracket + 1, i - startBracket - 1).Trim();
+                            if (tagContent.StartsWith("special", StringComparison.InvariantCultureIgnoreCase))
+                            {
+                                // Split the tag's contents into attributes
+                                Dictionary<string, string> attributeDict = new Dictionary<string, string>();
+                                foreach (string attr in TAG_ATTR_SEPARATION.Split(WHITESPACE.Replace(tagContent, " ")).Where(str => !string.IsNullOrWhiteSpace(str)).ToArray())
+                                {
+                                    string[] attrParts = attr.Split(new[] { '=' }, 2);
+                                    if (attrParts.Length == 2)
+                                    {
+                                        attributeDict.Add(attrParts[0].Trim(), attrParts[1].Trim());
+                                    }
+                                }
+
+                                // Find the entry in specialMappings and apply its effects
+                                bool found = false;
+                                foreach (SpecialMapping specialMapping in specialMappings)
+                                {
+                                    if (attributeDict.TryGetValue("special", out string keyword) && specialMapping.Keyword.Equals(keyword, StringComparison.InvariantCultureIgnoreCase))
+                                    {
+                                        // Found the entry in specialMappings
+                                        found = true;
+                                        foreach (SpecialMapping.SpecialEffects attr in specialMapping.Mappings.Keys)
+                                        {
+                                            if (attributeDict.TryGetValue(specialMapping.Mappings[attr], out string value))
+                                            {
+                                                // Found the effect's attribute on the tag
+                                                switch (attr)
+                                                {
+                                                    case SpecialMapping.SpecialEffects.Delay:
+                                                        if (float.TryParse(value, out float floatValue))
+                                                        {
+                                                            wait += advanceText ? 0 : floatValue;
+                                                        }
+                                                        else
+                                                        {
+                                                            Debug.LogError(
+                                                                "Error with tag: '" + tagContent + "'"
+                                                                + " | " + specialMapping.Mappings[attr] + " is not a float (" + value + ")"
+                                                                + " | The '" + attr + "' effect in Special Mappings requires the attribute value to be a valid float"
+                                                            );
+                                                        }
+                                                        break;
+                                                    default:
+                                                        break;
+                                                }
+                                            }
+                                            else
+                                            {
+                                                Debug.LogError(
+                                                    "Error with tag: '" + tagContent + "'"
+                                                    + " | " + specialMapping.Mappings[attr] + " is missing"
+                                                    + " | The '" + attr + "' effect of '" + keyword + "' in Special Mappings uses the '" + specialMapping.Mappings[attr] + "' attribute"
+                                                );
+                                            }
+                                        }
+                                        break;
+                                    }
+                                }
+
+                                if (!found)
+                                {
+                                    Debug.LogError("Error with tag: '" + tagContent + "' | Unable to find corresponding entry in Special Mappings for '" + attributeDict.GetValueOrDefault("special", null) + "'");
+                                }
+                            }
                         }
                         else
                         {
                             dialogueText.text = ApplyKeywordMappings((textBox.Text.Substring(0, i) + WrapTextInTag(textBox.Text.Substring(i), "<color=#00000000>", "</color>")).Replace("\\>", ">").Replace("\\<", "<"));
-                            float delay = advanceText ? FAST_CHAR_DELAY : CHAR_DELAY;
-                            if (delay >= Time.deltaTime || wait >= Time.deltaTime)
+                            wait += advanceText ? FAST_CHAR_DELAY : CHAR_DELAY;
+                            if (wait >= Time.deltaTime)
                             {
-                                wait = Mathf.Max(0, wait - Time.deltaTime);
                                 AkSoundEngine.PostEvent("Play_DX_Molly_Standard", gameObject);
-                                yield return new WaitForSecondsRealtime(delay);
-                            }
-                            else
-                            {
-                                wait += delay;
+                                yield return new WaitForSecondsRealtime(wait);
+                                wait = 0;
                             }
                         }
                     }
@@ -161,8 +244,8 @@ namespace Code.Scripts.Game.Managers
             advanceText = true;
         }
 
-        static readonly Regex ANGLE_BRACKETS = new Regex(@"(?<!\\)[><]");
-        static readonly string[] ATOMIC_TAGS = new string[] { "sprite", "button", "special" };
+        static readonly Regex ANGLE_BRACKETS = new Regex(@"(?<!\\)[><]", RegexOptions.IgnoreCase);
+        static readonly string[] ATOMIC_TAGS = new string[] { "sprite", "button", "special", "br" };
         private string WrapTextInTag(string text, string startTag, string endTag)
         {
             string output = "";
@@ -177,7 +260,15 @@ namespace Code.Scripts.Game.Managers
                 // startBracket is '>' or a non-bracket, and endBracket is '<' or a non-bracket
                 if ((startBracket == ">" || startBracket != "<") && (endBracket == "<" || endBracket != ">"))
                 {
-                    output += startTag + textParts[i] + endTag + endBracket;
+                    if (textParts[i].Length <= 0)
+                    {
+                        // No need to wrap empty
+                        output += endBracket;
+                    }
+                    else
+                    {
+                        output += startTag + textParts[i] + endTag + endBracket;
+                    }
                 }
                 else
                 {
@@ -192,7 +283,13 @@ namespace Code.Scripts.Game.Managers
                     }
                     else if (ATOMIC_TAGS.Any((tag) => textParts[i].TrimStart().StartsWith(tag, StringComparison.InvariantCultureIgnoreCase)))
                     {
-                        output = (output.Length == 0 ? "" : output.Remove(output.Length - 1)) + startTag + startBracket + textParts[i] + endBracket + endTag;
+                        // Atomic tags are like regular characters, so they don't need to end the wrapper tag and start a new one
+                        if (output.EndsWith(endTag + startBracket))
+                        {
+                            // The wrapper tag ended before this atomic tag, remove the end to continue it
+                            output = output.Substring(0, output.LastIndexOf(endTag + startBracket)) + startBracket;
+                        }
+                        output += textParts[i] + endBracket + endTag;
                     }
                     else
                     {
@@ -209,9 +306,17 @@ namespace Code.Scripts.Game.Managers
         {
             string output = text;
             InputManager.ControlScheme controlScheme = InputManager.GetControlScheme();
-            foreach (KeywordMapping keywordMapping in keywordMappings)
+            foreach (ButtonMapping buttonMapping in buttonMappings)
             {
-                output = output.Replace(keywordMapping.Keyword, keywordMapping.Mappings[controlScheme]);
+                string keyword = buttonMapping.Keyword.Replace("<button=", "").Replace(">", "");
+                Regex matchRegex = new Regex(@"<\s*button\s*=\s*" + keyword + ".*?>", RegexOptions.IgnoreCase);
+                output = matchRegex.Replace(output, buttonMapping.Mappings[controlScheme]);
+            }
+            foreach (SpecialMapping specialMapping in specialMappings)
+            {
+                string keyword = specialMapping.Keyword.Replace("<special=", "").Replace(">", "");
+                Regex matchRegex = new Regex(@"<\s*special\s*=\s*" + keyword + ".*?>", RegexOptions.IgnoreCase);
+                output = matchRegex.Replace(output, "");
             }
             return output;
         }
